8c2e18bf885bdffa7601d4014a1c0c6da6ac0de9
Andriy <andriy.galigin@gmail.com>
2026-02-10 21:13:00 +0200

    Ver1

--- linefollower.ino
+++ linefollower.ino
@@ -1,130 +1,80 @@
-/*motors*/
-int mr1=8;  //motor right 1
-int mr2=9;  //motor right 2
-int ml1=10;   //motor left 1
-int ml2=11; //motor left 2
-int enr=6; //A-enable/speed
-int enl=5; //B-enable/speed
+// ===== MOTORS =====
+int mr1=8;
+int mr2=9;
+int ml1=10;
+int ml2=11;
+int enr=6;
+int enl=5;
 
-/*sensors*/
-int sl2=2;   //sensor left2
-int sl1=3;   //sensor left
-int smid=4;   //sensor middle
-int sr1=7;   //sensor right
-int sr2=12;   //sensor right2
+// ===== SENSORS =====
+int sl2=2;
+int sl1=3;
+int smid=4;
+int sr1=7;
+int sr2=12;
 
-int led=13; //led
+int svl2, svl1, svm, svr1, svr2;
+int pattern;
 
-/*values*/
-int vspeed=100;    
-int tspeed=255;
-int tdelay=20;
-
-int svl2, svl1, svm, svr1, svr2 = 0; //sensors values
-
-
-void setup()
-{
+void setup() {
   pinMode(mr1,OUTPUT);
   pinMode(mr2,OUTPUT);
   pinMode(ml1,OUTPUT);
   pinMode(ml2,OUTPUT);
-  pinMode(led,OUTPUT);
-  pinMode(sl2,INPUT);
-  pinMode(sl1,INPUT);
-  pinMode(smid,INPUT);
-  pinMode(sr1,INPUT);
-  pinMode(sr2,INPUT);
-   
-  delay(5000);
+  pinMode(enr,OUTPUT);
+  pinMode(enl,OUTPUT);
+
+  pinMode(sl2,INPUT_PULLUP);
+  pinMode(sl1,INPUT_PULLUP);
+  pinMode(smid,INPUT_PULLUP);
+  pinMode(sr1,INPUT_PULLUP);
+  pinMode(sr2,INPUT_PULLUP);
+
+  Serial.begin(9600);
+  Serial.println("START");
 }
 
-void loop()
-{
-  svl2=digitalRead(sl2);
-  svl1=digitalRead(sl1);
-  svm=digitalRead(smid);
-  svr1=digitalRead(sr1);
-  svr2=digitalRead(sr2);
+void loop() {
+  svl2 = digitalRead(sl2) == LOW;
+  svl1 = digitalRead(sl1) == LOW;
+  svm  = digitalRead(smid) == LOW;
+  svr1 = digitalRead(sr1) == LOW;
+  svr2 = digitalRead(sr2) == LOW;
 
-  int pattern =
-  (svl2 << 4) |
-  (svl1 << 3) |
-  (svm << 2) |
-  (svr1 << 1) |
-  (svr2);
+  pattern = (svl2 << 4) |
+            (svl1 << 3) |
+            (svm  << 2) |
+            (svr1 << 1) |
+            (svr2);
+
+  Serial.print("Pattern BIN: ");
+  Serial.println(pattern, BIN);
 
   switch (pattern) {
-    case 0b00100: forward(); break;
-    case 0b01000: left(); break;
-    case 0b00010: right(); break;
-    case 0b00000: stop(); break;
-  }
+    case 0b00100: move(255, 255, 1,0,1,0); break; // forward
 
+    case 0b01100: move(255, 255, 0,1,1,0); break; //left
+    case 0b11100: move(255, 255, 0,1,1,0); break;
+    case 0b11000: move(255, 255, 0,1,1,0); break;
+    case 0b10000: move(255, 255, 0,1,1,0); break;
 
-  /*if(pattern == 0b00000)
-  {
-    forward(); //continue
-  }
+    case 0b00110: move(255, 255, 1,0,0,1); break; //right
+    case 0b00010: move(255, 255, 1,0,0,1); break;
+    case 0b00110: move(255, 255, 1,0,0,1); break;
+    case 0b00110: move(255, 255, 1,0,0,1); break;
 
-  if(pattern == 0b00000)
-  {
-    left(); //turn left
+    case 0b00000: move(255, 255, 0,1,0,1); break; //back
+    default: move(255, 255, 1,0,1,0); break; //def
   }
-  else if(pattern == 0b00000)
-  {
-    right(); //turn right
-  }
-  else if(pattern == 0b00000)
-  {
-    stop(); //stop
-  }*/
 }
 
-void forward()
-  {
-    digitalWrite(mr1,HIGH);
-    digitalWrite(mr2,LOW);
-    digitalWrite(ml1,HIGH);
-    digitalWrite(ml2,LOW);
-    analogWrite(enr,vspeed);
-    analogWrite(enl,vspeed);
-  } 
+void move(int speedL, int speedR, bool mrf, bool mrb, bool mlf, bool mlb)
+{
+  digitalWrite(mr1, mrf);
+  digitalWrite(mr2, mrb);
+  digitalWrite(ml1, mlf);
+  digitalWrite(ml2, mlb);
 
-void backward()
-  {
-    digitalWrite(mr1,LOW);
-    digitalWrite(mr2,HIGH);
-    digitalWrite(ml1,LOW);
-    digitalWrite(ml2,HIGH);
-    analogWrite(enr,vspeed);
-    analogWrite(enl,vspeed);
-  }
-
-void right()
-  {
-   digitalWrite(mr1,LOW);
-   digitalWrite(mr2,HIGH);
-   digitalWrite(ml1,HIGH);
-   digitalWrite(ml2,LOW);
-   analogWrite(enr,tspeed);
-   analogWrite(enl,tspeed);
-   delay(tdelay);
-  } 
-
-void left()
-  {
-   digitalWrite(mr1,HIGH);
-   digitalWrite(mr2,LOW);
-   digitalWrite(ml1,LOW);
-   digitalWrite(ml2,HIGH);
-   analogWrite(enr,tspeed);
-   analogWrite(enl,tspeed);
-   delay(tdelay);
-  }  
-
-void stop()
-  {
-    analogWrite(enr,0);
-    analogWrite(enl,0);
-  }
+  analogWrite(enr, speedR);
+  analogWrite(enl, speedL);
+}
